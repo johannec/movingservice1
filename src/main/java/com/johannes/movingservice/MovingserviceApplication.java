f7e610b473a670317556bcbf6a838cb467e73bbe
johannech <johannec@ifi.uio.no>
2019-07-08 13:07:12 +0200

    Initial commit

---
+++ src/main/java/com/johannes/movingservice/MovingserviceApplication.java
@@ -0,0 +1,13 @@
+package com.johannes.movingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovingserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovingserviceApplication.class, args);
+	}
+
+}
